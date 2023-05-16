fb1efe9bfc97857c58d84f9c893786c9b487454a
KNags <knageshwari07@gmail.com>
2023-05-16 12:13:22 +0530

    color changed

--- dom.js
+++ dom.js
@@ -1,9 +1,14 @@
-// var headerTitle=document.getElementById('header-title')
+ var headerTitle=document.getElementById('header-title')
 var header=document.getElementById('main-header')
-
-// headerTitle.style.borderBlock="solid 3px black"
 header.style.borderBlock="solid 3px black"
 
 var addTitle = document.querySelector("#main h2.title:first-of-type");
 addTitle.style.fontWeight = "bold";
 addTitle.style.color = "green";
+
+var items=document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';   
+}
