49dc56bc29f9f149caa2bb6d6d5f8cf3063d6705
KNags <knageshwari07@gmail.com>
2023-05-16 21:35:24 +0530

    queryselector done

--- dom.js
+++ dom.js
@@ -6,6 +6,7 @@ var addTitle = document.querySelector("#main h2.title:first-of-type");
 addTitle.style.fontWeight = "bold";
 addTitle.style.color = "green";
 
+//getelementbyclass
  var items=document.getElementsByClassName('list-group-item')
 items[2].style.backgroundColor='green';
 
@@ -21,3 +22,32 @@ list[1].style.backgroundColor='yellow';
 for(var i=0;i<list.length;i++){
     list[i].style.fontWeight='bold';   
 } 
+
+//queryselector
+var heading=document.querySelector('#main-header');
+heading.style.borderBlock  ='solid 4px blue';
+
+var input=document.querySelector('input');
+input.value='Enter the items';
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value='Enter';
+
+// var item=document.querySelector('.list=group-item')
+// item.style.color='red';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='lightgreen';
+}
+ 
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<even.length;i++)
+{
+    even[i].style.backgroundColor='skyblue';
+}
+
+const thirdItem = document.querySelector
+('#items li:nth-child(3)');
+thirdItem.style.display = 'none';
