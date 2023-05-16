12b5875742a9b6b2c4661853d59f2fcc8b835e6f
KNags <knageshwari07@gmail.com>
2023-05-16 11:33:52 +0530

    Finished

---
+++ dom.js
@@ -0,0 +1,9 @@
+// var headerTitle=document.getElementById('header-title')
+var header=document.getElementById('main-header')
+
+// headerTitle.style.borderBlock="solid 3px black"
+header.style.borderBlock="solid 3px black"
+
+var addTitle = document.querySelector("#main h2.title:first-of-type");
+addTitle.style.fontWeight = "bold";
+addTitle.style.color = "green";
---
+++ domcopy.js
@@ -0,0 +1,24 @@
+//Examine the docment oject//
+
+ //console.dir(document)
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.domain); //document.title=123;
+// console.log(document.doctype);
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10])
+///document.all[10].textContent="hello"
+// console.log(document.forms)
+// console.log(document.links)
+// console.log(document.images)
+
+// GEtelementbyid
+
+var headerTitle=document.getElementById('header-title');
+// console.log(headerTitle)
+//headerTitle.textContent="Hello"
+//headerTitle.innerText="Goodbye"
+// console.log(headerTitle.innerText)
+// headerTitle.innerHTML="<h3>Hello</h3>"
