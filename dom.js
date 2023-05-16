487722f1cd48deec721e3a6b684a8dd71111263e
KNags <knageshwari07@gmail.com>
2023-05-16 12:41:38 +0530

    new list added

--- dom.js
+++ dom.js
@@ -6,9 +6,18 @@ var addTitle = document.querySelector("#main h2.title:first-of-type");
 addTitle.style.fontWeight = "bold";
 addTitle.style.color = "green";
 
-var items=document.getElementsByClassName('list-group-item')
+ var items=document.getElementsByClassName('list-group-item')
 items[2].style.backgroundColor='green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';   
-}
+ for(var i=0;i<items.length;i++){
+     items[i].style.fontWeight='bold';   
+ }
+
+//getelementbytagname
+var list=document.getElementsByTagName('li')
+console.log(list[0])
+list[1].textContent='Option 1';
+list[1].style.backgroundColor='yellow';
+for(var i=0;i<list.length;i++){
+    list[i].style.fontWeight='bold';   
+} 
