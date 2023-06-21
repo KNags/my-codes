2d3c906c5b83cc0c1d44c0fac0a865661e822643
KNags <knageshwari07@gmail.com>
2023-06-21 11:50:57 +0530

    dom finished

--- dom.js
+++ dom.js
@@ -1,92 +1,44 @@
-
-const form = document.getElementById('addForm');
-const ItemList = document.getElementById('items');
-const filter = document.getElementById('filter');
-// form submit event
-
-form.addEventListener('submit', addItem);
-
-// Add Item
-function addItem(e){
-    e.preventDefault();
-   
-
-    // create new item for the input 
-    const newItem = document.getElementById('item').value;
-    const desCription = document.getElementById('description').value;
-    // ceate new li 
-    const li = document.createElement('li');
-
-    // add class to li
-    li.className = 'list-group-item';
-    
-    const newText = document.createTextNode(newItem);
-    const descriptionNode = document.createTextNode(' '+desCription);
-    // append the newitem to li
-    li.appendChild(newText);
-    li.appendChild(descriptionNode);
-    
-    //append the li into ItemList
-    ItemList.appendChild(li);
-   
-
-    console.log(li);
-    // create a delete button
-    const delbtn = document.createElement('button');
-    // add class to the button
-    delbtn.className = 'btn btn-danger btn-sm float-right delete';
-    //add text to the buttton
-    delbtn.appendChild(document.createTextNode('X'));
-    // add delbtn to the list
-    li.appendChild(delbtn);
-    ItemList.appendChild(li);
-
-    // create a edit button
-    const editbtn = document.createElement('button');
-    editbtn.className = 'btn btn-primary btn-sm float-right';
-    editbtn.appendChild(document.createTextNode('Edit'));
-    li.appendChild(editbtn);
-    ItemList.appendChild(li);
-
-    
-}
-
-// craete a remove function 
-ItemList.addEventListener('click' , removeItem);
-
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-       if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        ItemList.removeChild(li);
-       }
+function saveToLocalStorage(event){
+    event.preventDefault();
+    const name = event.target.username.value;
+    const email = event.target.email.value;
+    const phonenumber = event.target.phonenumber.value;
+    //localStorage.setItem('name',name);
+    //localStorage.setItem('email',email);
+    //localStorage.setItem('phonenumber',phonenumber);
+    const obj={
+        name,
+        email,
+        phonenumber,
     }
+    localStorage.setItem(obj.email ,JSON.stringify(obj));
+    showUserOnScreen(obj);
 }
+function showUserOnScreen(obj){
+const parentElem = document.getElementById('listofitems')
+const childElem = document.createElement('li')
+// parentElem.innerHTML=parentElem.innerHTML+`${obj.name} - ${obj.email} - ${obj.phonenumber}`
+// parentElem.appendChild(childElem)
+childElem.textContent=obj.name + '-' + obj.email + '-' + obj.phonenumber
 
-// filter event
-filter.addEventListener('keyup' , filterItem);
-
-// filterItem function
-
-function filterItem(e){
-    // converts text to lower
-    var text = e.target.value.toLowerCase();
-    // get li's
-    var items = ItemList.getElementsByTagName('li');
-    // converting to an array of html collection
-    Array.from(items).forEach(function(item){
-        var itemName = item.firstChild.textContent;
-        var descriptionName = item.childNodes[1].textContent;
-
-        if(itemName.toLowerCase().indexOf(text) != -1){
-            item.style.display = 'block';
-        }else{
-            item.style.display = 'none';
-        }
-        if(descriptionName.toLowerCase().indexOf(text) != -1){
-            item.style.display = 'block';
-        }else{
-            item.style.display = 'none';
-        }
-    })
+const deleteButton= document.createElement('input')
+deleteButton.type = "button"
+deleteButton.value = "Delete"
+deleteButton.onclick = () => {
+    localStorage.removeItem(obj.email)
+    parentElem.removeChild(childElem)
+}
+const editButton = document.createElement('input');
+editButton.type = 'button';
+editButton.value = 'Edit';
+editButton.onclick = () => {
+localStorage.removeItem(childElem);
+parentElem.removeChild(childElem);
+document.getElementById('username').value = obj.name;
+document.getElementById('email').value = obj.email;
+document.getElementById('phoneNumber').value = obj.phonenumber;
+}
+childElem.appendChild(deleteButton)
+childElem.appendChild(editButton)
+parentElem.appendChild(childElem)
 }
